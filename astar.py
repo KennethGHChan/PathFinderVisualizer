2a3e00b199490e6e3b1d2a4d471335b48aa4ccea
KennethGHChan <kennethghchan@gmail.com>
2022-11-07 20:40:07 -0500

    Adding Display

--- astar.py
+++ astar.py
@@ -7,3 +7,14 @@ WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 # Caption for the display
 pygame.display.set_caption("A* Path Finding Algorithm")
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
