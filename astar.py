677908e48800e5ac9cc4d17b782aa9965cc61b39
KennethGHChan <kennethghchan@gmail.com>
2022-11-07 20:26:30 -0500

    setup

---
+++ astar.py
@@ -0,0 +1,9 @@
+import pygame
+import math
+from queue import PriorityQueue
+
+# Setting up the display
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+# Caption for the display
+pygame.display.set_caption("A* Path Finding Algorithm")
